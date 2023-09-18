6259d6918bffb50487c074b54d8d11aaee844f33
Roshan <roshanrahman6399@gmail.com>
2023-09-18 15:00:56 +0530

    Add search bar

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,5 @@
 import { Query } from "@/types/query";
+import { useState, useMemo } from "react";
 
 export interface ListProps {
   queries: Query[];
@@ -18,14 +19,43 @@ export default function List({
   onClick,
   onCreate,
 }: ListProps) {
+  const [searchInput, setSearchInput] = useState("");
+
+  const filteredQueries = useMemo(() => {
+    if (searchInput.length > 0) {
+      return queries.filter((query) => query.name.includes(searchInput));
+    }
+    return queries;
+  }, [queries, searchInput]);
+
   return (
     <div className="w-full border-gray-100 border-r-2 h-full">
       <div className="text-sm tracking-wide text-slate-500  py-2 px-4 border-b border-gray-200 select-none mt-2">
         YOUR QUERIES
       </div>
 
+      <div className="flex gap-2 px-2 py-2">
+        <input
+          type="text"
+          placeholder="Search"
+          className="flex-1 border-slate-300 border-2 rounded-md py-1 px-2 text-sm"
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+        <button
+          className="text-sm rounded bg-blue-600 py-1 px-3 text-white"
+          onClick={() => {
+            onCreate();
+          }}
+        >
+          New
+        </button>
+      </div>
+
       <ul className="list-none">
-        {queries.map((query) => {
+        {filteredQueries.map((query) => {
           const isSelected = selectedQueryId === query.id;
           return (
             <li key={query.id} className="w-full">
